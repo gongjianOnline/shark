5afe81a55b01106c82b493f1b28e517b80db08d3
李承斌 <15031617367@163.com>
2020-09-22 17:14:24 +0800

    增加了备注

--- src/components/isAlert.vue
+++ src/components/isAlert.vue
@@ -34,6 +34,7 @@
       }
     },
     mounted() {
+      //获得浏览设备是否是移动端
       if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
         this.isAlert = false
       }else{
